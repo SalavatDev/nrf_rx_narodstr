98e75de2adab3fe5ceb2227349c81dbb827697c2
SalavatDev <k_salavat@bk.ru>
2024-12-19 00:32:44 +0500

    Отредактированы приоритеты прерываний в кубе

--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -240,7 +240,7 @@ void TIM3_IRQHandler(void)
   /* USER CODE BEGIN TIM3_IRQn 0 */
   
   /* USER CODE END TIM3_IRQn 0 */
-  //HAL_TIM_IRQHandler(&htim3);
+  HAL_TIM_IRQHandler(&htim3);
   /* USER CODE BEGIN TIM3_IRQn 1 */
   
 	
